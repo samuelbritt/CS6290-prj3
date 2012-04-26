921b712c98f0ba34b61a9caf602822b86623dea0
Sam Britt <samuelbritt@gmail.com>
2012-04-26 13:45:49 -0400

    add simulation output

--- runs.py
+++ runs.py
@@ -10,16 +10,19 @@ import subprocess
 
 protocols = ("MI", "MSI", "MESI", "MOSI", "MOESI", "MOESIF")
 
+prog = "./sim_trace"
 trace_dir = "./traces"
 output_dir = "./runs"
-prog = "./sim_trace"
+run_dir = os.path.join(output_dir, "runs")
+data_dir = os.path.join(output_dir, "data")
 
 def run_simulation(trace, protocol):
     input_trace = os.path.join(trace_dir, trace)
     output_filename = "{}_{}.run".format(trace, protocol)
-    output = os.path.join(output_dir, output_filename)
+    output = os.path.join(run_dir, output_filename)
     output_file = open(output, "w+")
     cmd = (prog, "-t", input_trace, "-p", protocol)
+    print "Running", " ".join(cmd)
     subprocess.call(cmd, stderr=subprocess.STDOUT, stdout=output_file)
     output_file.seek(0)
     return output_file
@@ -37,7 +40,7 @@ def parse_results(trace, protocol, results_file):
                 data[s] = extract_int_from_results(line)
 
     data_filename = "{}.csv".format(trace)
-    data_filename = os.path.join(output_dir, "data", data_filename)
+    data_filename = os.path.join(data_dir, data_filename)
     data_file = open(data_filename, "a")
     data_file.write(", ".join((protocol,
                                str(data["Run Time"]),
@@ -50,6 +53,15 @@ def parse_results(trace, protocol, results_file):
     data_file.write("\n")
 
 if __name__ == '__main__':
+    try:
+        os.mkdir(run_dir)
+    except OSError:
+        pass
+    try:
+        os.mkdir(data_dir)
+    except OSError:
+        pass
+
     traces = [os.path.join(trace_dir, d) for d in os.listdir(trace_dir)]
     traces = sorted(filter(os.path.isdir, traces))
     trace_names = [os.path.basename(d) for d in traces]
